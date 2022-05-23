fecb386367e8d6952602b5dc6653dcfd696f538a
lamaboshi <lamaboshi.99@gmail.com>
2022-05-23 18:48:13 +0300

    Brand Repo

---
+++ MarfulApi/MarfulApi/Data/BrandRepo.cs
@@ -0,0 +1,30 @@
+﻿using MarfulApi.Infrastructure;
+using MarfulApi.Model;
+
+namespace MarfulApi.Data
+{
+    public class BrandRepo : IBrand
+    {
+        private readonly MarfulDbContext _db;
+        public BrandRepo(MarfulDbContext db)
+        {
+            _db = db;
+        }
+        public IQueryable<Brand> GetBrands => throw new NotImplementedException();
+
+        public void Delte(int IdBrand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Brand GetBrand(int IdBrand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MarfulApi/MarfulApi/Infrastructure/IBrand.cs
@@ -0,0 +1,12 @@
+﻿using MarfulApi.Model;
+
+namespace MarfulApi.Infrastructure
+{
+    public interface IBrand
+    {
+        public void Save();
+        public IQueryable<Brand> GetBrands { get;  }
+        public void Delte(int IdBrand);
+        public Brand GetBrand(int IdBrand);
+    }
+}
